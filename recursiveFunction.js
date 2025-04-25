6b64bd8547f17515d06c673b0b44ec88f7a8744c
BryJime <bry.jimenex@gmail.com>
2025-04-24 19:14:07 -0700

    Functions and Scope - 4

--- recursiveFunction.js
+++ recursiveFunction.js
@@ -14,3 +14,22 @@ function reverseString(string){
 
 
 console.log(reverseString("Hello"));
+
+
+
+/* COMMENTS FOR Practice Solution: Functions and Scope - 4 ASSIGNMENT:
+
+I notice that in the practice solution my base case is different. 
+In my own code I return the first character in the string at the index 0. 
+And when the recursive case is called it slices the character at index 0 
+and returns all the characters after the 1st index peeling off one character at a time. 
+The characters taken out are added recursively, reversing the string. 
+In the solution the recursive case returns the last character in the length of the 
+string and then adds the rest of the string recursively by slicing the string by each 
+character. 
+
+This tells me that even though my code was different, the solution offered a 
+different approach that I wouldn't have thought of. They ultimately do the same thing 
+but having a different perspective is interesting to see. 
+
+*/
